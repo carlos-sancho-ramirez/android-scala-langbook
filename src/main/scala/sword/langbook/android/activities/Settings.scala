4e97f202357d70886731e8ce6271dde7500ca16e
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2016-04-14 16:17:38 +0200

    Add settings screen to control database status

---
+++ src/main/scala/sword/langbook/android/activities/Settings.scala
@@ -0,0 +1,41 @@
+package sword.langbook.android.activities
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
+import sword.langbook.android.R
+
+object Settings {
+  private val className = "sword.langbook.android.activities.Settings"
+
+  def openWith(activity :Activity, requestCode :Int = 0) = {
+    val intent = new Intent()
+    intent.setClassName(activity, className)
+
+    if (requestCode > 0) activity.startActivityForResult(intent, requestCode)
+    else activity.startActivity(intent)
+  }
+}
+
+class Settings extends BaseActivity {
+
+  override def onCreate(savedInstanceState :Bundle) :Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.settings)
+
+    findViewById(R.id.importDatabaseButton).setOnClickListener(new OnClickListener {
+      override def onClick(v: View): Unit = {
+        Toast.makeText(Settings.this, getString(R.string.unsupportedFeature), Toast.LENGTH_SHORT).show()
+      }
+    })
+
+    findViewById(R.id.exportDatabaseButton).setOnClickListener(new OnClickListener {
+      override def onClick(v: View): Unit = {
+        Toast.makeText(Settings.this, getString(R.string.unsupportedFeature), Toast.LENGTH_SHORT).show()
+      }
+    })
+  }
+}
