9b205a5ec6e173583273e7345a7ae0d54e74276b
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2016-04-14 18:44:07 +0200

    Allow importing external databases

--- src/main/scala/sword/langbook/android/activities/Settings.scala
+++ src/main/scala/sword/langbook/android/activities/Settings.scala
@@ -1,12 +1,16 @@
 package sword.langbook.android.activities
 
+import java.io._
+
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Toast
 import sword.langbook.android.R
+import sword.langbook.android.db.SQLiteStorageManager
 
 object Settings {
   private val className = "sword.langbook.android.activities.Settings"
@@ -42,7 +46,54 @@ class Settings extends BaseActivity {
   override def onActivityResult(requestCode: Int, resultCode: Int, data: Intent) = {
     if (resultCode == Activity.RESULT_OK && requestCode == RequestCodes.pickFile) {
       val path = data.getStringExtra(BundleKeys.filePath)
-      Toast.makeText(this, s"Should import from $path", Toast.LENGTH_SHORT).show()
+      val message = {
+        if (importDatabaseFromPath(path)) R.string.databaseImportSuccess
+        else R.string.databaseImportFailure
+      }
+      Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+  }
+
+  private def importDatabaseFromPath(path: String): Boolean = {
+    val inFile = new File(path)
+    val outFile = getDatabasePath(SQLiteStorageManager.dbName)
+
+    try {
+      val inStream = new FileInputStream(inFile)
+
+      try {
+        val outStream = new FileOutputStream(outFile)
+
+        try {
+          val buffer = new Array[Byte](4096)
+          var count = 0
+
+          do {
+            count = inStream.read(buffer)
+            if (count > 0) {
+              outStream.write(buffer, 0, count)
+            }
+          }
+          while (count >= 0)
+          true
+        }
+        catch {
+          case _: IOException =>
+            // Nothing can be done
+            false
+        }
+        finally {
+          outStream.close()
+        }
+      }
+      finally {
+        inStream.close()
+      }
+    }
+    catch {
+      case _: FileNotFoundException =>
+        // Nothing can be done
+        false
     }
   }
 }
